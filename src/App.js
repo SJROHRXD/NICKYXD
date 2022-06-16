6b39bb22128a378d4aeadaa8ede7b342bfc59ca1
SJRXD <rohr.olasz@gmail.com>
2022-06-15 19:35:34 -0700

    please god make zakk stop

--- src/App.js
+++ src/App.js
@@ -15,16 +15,29 @@ function App() {
       <h1>Private View</h1>
         <Router>
           {/* how to go, when */}
+
+          {/* auth token, if true, then return these routes, if else return unprotected routes, you wanna handle this in navbar */}
+
           <Routes>
             {/* declaring what urls are */}
             <Route path="/login" authToken={authToken} setAuthtoken={setAuthtoken} element={<Login />} />
-            <Route path="/logged" element={<TestimonialListAdmin />} />
+            <Route path="/loggedin" authToken={authToken} element={<TestimonialListAdmin />} />
             <Route path="/notloggedin" element={<TestimonialList />} />
-            <Route path="/*" element={<Redirect />} />
+            <Route path="/" element={<Redirect />} />
           </Routes>
         </Router>
     </div>
   );
 };
+
+// jesus christ zakk lost me eight years ago
+
+
+// we want a redirect, theoretically in case they manual punch in the url
+// then redirect
+// react redirect google it
+
+// conditional rendering in navbar
+// turnerrarryryry
 
 export default App;
--- src/components/TestimonialListAdmin.jsx
+++ src/components/TestimonialListAdmin.jsx
@@ -1,15 +1,9 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function aNameRightnow() {
-    const [authToken, setAuthtoken] = useState(false);
+export default function TestimonialListAdmin({authToken
+    () => {
 
-    return (
-        <div className='Appidk'>
-            <h1>Nick's bs</h1>
-        </div>
-    )
-};
+}
 
-
-// redirect
+)
