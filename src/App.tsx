14e437cada5c20aa5f095fa729b808476f889c86
SJRXD <rohr.olasz@gmail.com>
2022-11-23 20:00:04 -0800

    initial pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { Home } from "./Home";
+import { Resume } from "./Resume";
+import { Projects } from "./Projects";
+
 function App() {
   return (
     <div className="App">
---
+++ src/pages/Resume.tsx
@@ -0,0 +1,1 @@
+// jobs render here
